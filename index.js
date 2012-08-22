02157294f0366eb582f37264f0a35f5abe63b6c9
Carlos Rodriguez <carlos@s8f.org>
2012-08-22 00:53:46 -0700

    initial code

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-index.js
+var createProtocol = require('./protocol')
+  , request = require('request')
+  , http = require('http')
+  , https = require('https')
+
+exports.attach = function (options) {
+  this.request = request.defaults({
+    httpModules: { 'amino:': createProtocol(options), 'http:': http, 'https:': https },
+    json: true
+  });
+};

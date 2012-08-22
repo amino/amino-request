6870dd62ac362a014a186603757ecbf20c44a5b7
Carlos Rodriguez <carlos@s8f.org>
2012-08-22 11:00:13 -0700

    new request api

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-var createProtocol = require('./lib/protocol')
-  , request = require('request')
+var getProtocol = require('./lib/protocol')
   , http = require('http')
   , https = require('https')
+  , request = require('request')
   , Spec = require('amino-spec')
 
 exports.attach = function (options) {
   var customRequest = request.defaults({
-    httpModules: { 'amino:': createProtocol(options), 'http:': http, 'https:': https },
+    httpModules: { 'amino:': getProtocol(options), 'http:': http, 'https:': https },
     json: true
   });
+
   this.request = function (service, path, cb) {
     if (typeof service === 'object' || (typeof service === 'string' && service.indexOf('://') !== -1)) {
       cb = path;
       return customRequest(service, cb);
     }
-    var spec = new Spec(service)
-      , opts = {
+    var spec = new Spec(service);
+    var opts = {
       url: 'amino://' + spec.service + path,
       headers: { 'X-Amino-Version': spec.version }
-    }
+    };
 
     return customRequest(opts, cb);
   };
